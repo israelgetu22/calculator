2c0d7bd4575bcfec3a0a2346a10ba348deb384ae
Isarel Getu <israelgetu101@gmail.com>
2022-02-12 22:51:44 -0600

    Done

--- calc.js
+++ calc.js
@@ -1,21 +1,35 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+reader.question("What would you like to calculate?", function (input) {
+  tokens = input.split(" ");
 
+  mathSymbol = tokens[0];
+  num1 = Number(tokens[1]);
+  num2 = Number(tokens[2]);
 
-reader.question("What would you like to calculate?", function(input){
-	tokens = input.split(' ');
-	
-	mathSymbol = tokens[0];
-	num1 = Number(tokens[1]);
-	num2 = Number(tokens[2]);
+  // This line closes the connection to the command line interface.
 
+  if (mathSymbol === "+") {
+    console.log(num1 + num2);
+  }
+  if (mathSymbol === "-") {
+    console.log(num1 - num2);
+  }
+  if (mathSymbol === "*") {
+    console.log(num1 * num2);
+  }
+  if (mathSymbol === "/") {
+    console.log(num1 / num2);
+  }
+  if (mathSymbol === "sqrt") {
+    console.log(Math.sqrt(num1));
+    console.log(Math.sqrt(num2));
+  }
 
-	// This line closes the connection to the command line interface.
-	reader.close()
-
+  reader.close();
 });
